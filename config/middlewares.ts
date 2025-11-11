a321ee2a0f3dac9fa3cc1bb173d51b183bc9f24c
Christian Kuri <christian.kuri.martinez@gmail.com>
2025-11-11 18:09:40 +0000

    fix: configure CSP to allow CloudFront images in admin panel

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'd36o3waxm537og.cloudfront.net', // Your CloudFront domain
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'd36o3waxm537og.cloudfront.net', // Your CloudFront domain
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
