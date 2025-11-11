1c661a188d127c25033a828f0e88ca1925f232c5
Christian Kuri <christian.kuri.martinez@gmail.com>
2025-11-11 18:11:30 +0000

    refactor: use CDN_URL env var for CSP instead of hardcoding domain

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,37 +1,43 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'd36o3waxm537og.cloudfront.net', // Your CloudFront domain
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'd36o3waxm537og.cloudfront.net', // Your CloudFront domain
-          ],
-          upgradeInsecureRequests: null,
+export default ({ env }) => {
+  // Extract domain from CDN_URL if it exists
+  const cdnUrl = env('CDN_URL');
+  const cdnDomain = cdnUrl ? new URL(cdnUrl).hostname : null;
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              ...(cdnDomain ? [cdnDomain] : []),
+            ],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              ...(cdnDomain ? [cdnDomain] : []),
+            ],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
